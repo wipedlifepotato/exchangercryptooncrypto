f4cab8e9280ab5aa2d634c7cdf5ec17deac2a80d
nonlin-lin-chaos-order-etc-etal <hypnosis.i2p@gmail.com>
2021-05-17 00:42:46 +0800

    fixing config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
-function getenv_helper(key, default_value) {
-	return getenv(key, true) ?: getenv(key) ?: default_value;
+function getenv_helper($key, $default_value) {
+	return getenv($key, true) ?: getenv($key) ?: $default_value;
 }
 
-function var(em_key, env_var_key, default_value) {
-	define(em_key, getenv_helper(env_var_key, default_value));
+function var($em_key, $env_var_key, $default_value) {
+	define($em_key, getenv_helper($env_var_key, $default_value));
 }
 
 var('MYSQL_HOST', 'ENV_EM_MYSQL_HOSTNAME', '127.0.0.1');
