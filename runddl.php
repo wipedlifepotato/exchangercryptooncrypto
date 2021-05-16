fae1a261cf69c4b888408fa7e12d4006fa44ec2a
nonlin-lin-chaos-order-etc-etal <hypnosis.i2p@gmail.com>
2021-05-17 02:46:19 +0800

    work on runddl.php

--- runddl.php
+++ runddl.php
@@ -1,5 +1,8 @@
 <?php
 	require_once('config.php');
+
+	if (ENV_EM_ENABLE_RUNDDL!="true") die('runddl disabled');
+
 	require_once('sys/sql.php');
 	require_once('sys/util.php');
 
@@ -11,16 +14,20 @@
 
 		function run_ddl() {
     		$dbdump = file_get_contents(RUNDDL_DBDUMP_FILENAME);
-			if (!$dbdump) die("failed to read the DDL dump file $RUNDDL_DBDUMP_FILENAME");
+			if (!$dbdump) die('failed to read the DDL dump file '.RUNDDL_DBDUMP_FILENAME);
 			$dbdump_statements = explode(';', $dbdump);
-
-		$u = new users( $sql );
-
+			foreach ($dbdump_statements as $sql_statement) {
+				print ("exec $sql_statement... ");
+				$result = $this->sql->doSQL($sql_statement);
+				print ("result $result;<br>
+");
+			}
+			print("<p>all done</p>");
 		}
 	}
 
 	if (RUNDDL_PASSWORD != $_GET['RUNDDL_PASSWORD']) {
-		sleep(3);
+		sleep(1);
 		die("Not authorized");
 	}
 
