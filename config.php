93920bfa86a923d971734f31d923c4dac1a60181
nonlin-lin-chaos-order-etc-etal <hypnosis.i2p@gmail.com>
2021-05-17 00:49:48 +0800

    fixed a typo at config.php

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ config_var('MYSQL_HOST', 'ENV_EM_MYSQL_HOSTNAME', '127.0.0.1');
 config_var('MYSQL_PASS', 'ENV_EM_MYSQL_PASSWORD', 'secretpassword');
 config_var('MYSQL_USERNAME', 'ENV_EM_MYSQL_USERNAME', 'root');
 config_var('MYSQL_DB', 'ENV_EM_MYSQL_DB', 'emdb');
-config_var('CHAT_REFRESH_SECONDS', , 'ENV_EM_CHAT_REFRESH_SECONDS', 20);
+config_var('CHAT_REFRESH_SECONDS', 'ENV_EM_CHAT_REFRESH_SECONDS', 20);
 config_var('MAX_ADDRESS_FOR_WALLET', 'ENV_EM_MAX_ADDRESS_FOR_WALLET', 15);
 
 ?>
