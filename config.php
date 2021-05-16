c38fbaa3938cfd11df171b8a0216d093525e498f
nonlin-lin-chaos-order-etc-etal <hypnosis.i2p@gmail.com>
2021-05-17 00:16:18 +0800

    php configs now taken from the os env vars

--- config.php
+++ config.php
@@ -1,8 +1,18 @@
 <?php
-define("MYSQL_HOST", "localhost");
-define("MYSQL_PASS", "HAga8wA45FEHKUVmFmGMxjCdfiCzNN");
-define("MYSQL_USERNAME", "root");
-define("MYSQL_DB", "ancms");
-define("CHAT_REFRESH_SECONDS", 20);
-define("MAX_ADDRESS_FOR_WALLET", 15);
+
+function getenv_helper(key, default_value) {
+	return getenv(key, true) ?: getenv(key) ?: default_value;
+}
+
+function var(em_key, env_var_key, default_value) {
+	define(em_key, getenv_helper(env_var_key, default_value));
+}
+
+var('MYSQL_HOST', 'ENV_EM_MYSQL_HOSTNAME', '127.0.0.1');
+var('MYSQL_PASS', 'ENV_EM_MYSQL_PASSWORD', 'secretpassword');
+var('MYSQL_USERNAME', 'ENV_EM_MYSQL_USERNAME', 'root');
+var('MYSQL_DB', 'ENV_EM_MYSQL_DB', 'emdb');
+var('CHAT_REFRESH_SECONDS', , 'ENV_EM_CHAT_REFRESH_SECONDS', 20);
+var('MAX_ADDRESS_FOR_WALLET', 'ENV_EM_MAX_ADDRESS_FOR_WALLET', 15);
+
 ?>
